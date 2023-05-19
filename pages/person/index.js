9db0907d5328644426bf2703428103cb839453b9
baimianii <13536314199@163.com>
2023-05-19 15:45:24 +0800

    拼接个人页面

---
+++ pages/person/index.js
@@ -0,0 +1,44 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+        // isShow:{
+        //     show:"none",
+        //     isTrue:true
+        // }
+  },
+//   a(){
+//       if(this.data.isShow.isTrue){
+//           this.setData({
+//           isShow:{
+//               show:"block",
+//               isTrue:false
+//           }
+//       })
+//       }else{
+//         this.setData({
+//             isShow:{
+//                 show:"none",
+//                 isTrue:true
+//             }
+//       })
+//     }
+      
+//   },
+  // 事件处理函数
+  bindViewTap() {
+    
+  },
+  onLoad() {
+    
+  },
+  getUserProfile(e) {
+    
+    
+  },
+  getUserInfo(e) {
+   
+  }
+})
